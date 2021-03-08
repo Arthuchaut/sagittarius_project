428f72bc6d5efcdac47298c87207694e35dccc9e
Arthuchaut <arthuchaut@gmail.com>
2021-03-08 21:16:31 +0100

    Added DefaultTag model test in transaction

---
+++ transaction/tests/models/test_default_tag.py
@@ -0,0 +1,24 @@
+import pytest
+from transaction.models import DefaultTag
+
+
+class TestDefaultTag:
+    '''The User model test class.
+    '''
+
+    @pytest.mark.django_db(transaction=True)
+    @pytest.mark.parametrize(
+        'name, icon', [
+            ('foo', None),
+            ('bar', 'baz'),
+            pytest.param(None, 'baz', marks=pytest.mark.xfail),
+        ]
+    )
+    def test_create(self, name: str, icon: str) -> None:
+        '''Test the user_tag creation.
+        '''
+
+        default_tag: DefaultTag = DefaultTag.objects.create(
+            name=name, icon=icon
+        )
+        assert default_tag
